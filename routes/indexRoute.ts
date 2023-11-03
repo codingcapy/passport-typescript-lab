318a29fadb2a2421807b82876e5ad81112fe917a
Paul K <spkim0921@gmail.com>
2023-11-02 17:06:58 -0700

    8th commit
    implemented live sessions view in dashboard

--- routes/indexRoute.ts
+++ routes/indexRoute.ts
@@ -7,9 +7,25 @@ router.get("/", (req, res) => {
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    user: req.user,
-  });
+  const sessions = Object.keys((req as any).sessionStore.sessions).map((key) => JSON.parse((req as any).sessionStore.sessions[key]));
+  res.render("dashboard", { user: req.user, sessions: sessions });
+});
+
+router.get('/sessions', (req, res) => {
+  const sessions = Object.keys((req as any).sessionStore.sessions).map((key) => JSON.parse((req as any).sessionStore.sessions[key]));
+  res.send(sessions);
+});
+
+router.get('/killsession/:sessionId', (req, res) => {
+  const sessionId = req.params.sessionId as string;
+  const sessionStore = req.sessionStore as any;
+
+  if (sessionStore.sessions && sessionStore.sessions[sessionId]) {
+    delete sessionStore.sessions[sessionId];
+    res.send('Session killed successfully.');
+  } else {
+    res.send('Session not found.');
+  }
 });
 
 export default router;
