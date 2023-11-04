280eeeb7210617d3f520a9795a7727c9666a4746
Paul K <spkim0921@gmail.com>
2023-11-03 17:42:06 -0700

    11th commit
    testing session revoker

--- routes/indexRoute.ts
+++ routes/indexRoute.ts
@@ -7,26 +7,37 @@ router.get("/", (req, res) => {
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  const sessions = Object.keys((req as any).sessionStore.sessions).map((key) => JSON.parse((req as any).sessionStore.sessions[key]));
+  // const sessions = Object.keys((req as any).sessionStore.sessions).map((key) => JSON.parse((req as any).sessionStore.sessions[key]));
+  // const sessions = Object.values((req as any).sessionStore.sessions).map((session: any) => {
+  //     const parsedSession = JSON.parse(session);
+  //     return { id: parsedSession.id, data: parsedSession };
+  // });
+  const sessions = Object.keys((req as any).sessionStore.sessions)
   res.render("dashboard", { user: req.user, sessions: sessions });
 });
 
 //testing sessions display
 router.get('/sessions', (req, res) => {
-  const sessions = Object.keys((req as any).sessionStore.sessions).map((key) => JSON.parse((req as any).sessionStore.sessions[key]));
+  const sessions = Object.keys((req as any).sessionStore.sessions)
+  const sessions2 = Object.values((req as any).sessionStore.sessions)
+  const sessions3 = Object((req as any).sessionStore.sessions)
   res.send(sessions);
 });
 
-router.get('/killsession/:sessionId', (req, res) => {
-  const sessionId = req.params.sessionId as string;
-  const sessionStore = req.sessionStore as any;
-
-  if (sessionStore.sessions && sessionStore.sessions[sessionId]) {
-    delete sessionStore.sessions[sessionId];
-    res.send('Session killed successfully.');
-  } else {
-    res.send('Session not found.');
-  }
+router.post('/revokesession/:session', (req, res) => {
+  const sessionId = req.params.session as string;
+  const sessions = Object.keys((req as any).sessionStore.sessions)
+  sessions.map((key) => {
+      if (key === sessionId) {
+          Object((req as any).sessionStore.sessions).container[key].destroy((err: any) => {
+              if (err) {
+                  res.send('Error revoking session.');
+              } else {
+                  res.redirect('/dashboard');
+              }
+          })
+      }
+  })
 });
 
 export default router;
